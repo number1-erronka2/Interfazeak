694cedb98410f92d4141827fb082bcbb299ae47f
Erlantz <96289199+erlgr@users.noreply.github.com>
2023-02-14 13:32:26 +0100

    Deleted legacy project

--- WebAplikazioa/WebAplikazioa/Models/LangileaModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAplikazioa.Models {
-    public class LangileaModel { //HomeController.cs, Jokoak.cshtml eta Jokoa.cshtml fitxategietan erabiltzeko behar dena
-        public string Email { get; set; }
-        public string Izena { get; set; }
-        public string User { get; set; }
-        public DateOnly JaiotzaData { get; set; }
-        public int Taldea { get; set; }
-    }
-}
--- WebAplikazioa/WebAplikazioa/Models/PartidaModel.cs
+++
@@ -1,8 +0,0 @@
-namespace WebAplikazioa.Models {
-    public class PartidaModel { //HomeController.cs, Jokoak.cshtml eta Jokoa.cshtml fitxategietan erabiltzeko behar dena
-        public int Id { get; set; }
-        public string User { get; set; }
-        public float Puntuazioa { get; set; }
-        public DateTime Data { get; set; }
-    }
-}
