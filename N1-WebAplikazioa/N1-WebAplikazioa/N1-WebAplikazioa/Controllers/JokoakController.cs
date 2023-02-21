da098e894656e3c85ea94b0184dc7b9b64371356
Erlantz <96289199+erlgr@users.noreply.github.com>
2023-02-21 13:30:25 +0100

    Jokoen orria + docFX init

---
+++ N1-WebAplikazioa/N1-WebAplikazioa/N1-WebAplikazioa/Controllers/JokoakController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace N1_WebAplikazioa.Controllers; 
+
+public class JokoakController : Controller { 
+    public IActionResult Index() { 
+        return View(); 
+    }
+}
